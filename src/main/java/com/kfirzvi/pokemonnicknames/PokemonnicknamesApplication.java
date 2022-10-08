fe49cc77da8b3bb37b5cd6e1887cb3e20aa2ccb7
Kfir Zvi <zvikfir10@gmail.com>
2022-10-08 23:35:16 +0300

    init commit

---
+++ src/main/java/com/kfirzvi/pokemonnicknames/PokemonnicknamesApplication.java
@@ -0,0 +1,13 @@
+package com.kfirzvi.pokemonnicknames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonnicknamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonnicknamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kfirzvi/pokemonnicknames/PokemonnicknamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfirzvi.pokemonnicknames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonnicknamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
